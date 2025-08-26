cc0c643c2ec41268538adb4ea71d657856e6aeb1
DiamonDoughnut <calebssmith@hotmail.com>
2025-08-25 20:11:28 -0500

    updating and prepping for gh pages launch

--- src/gencontent.py
+++ src/gencontent.py
@@ -2,7 +2,7 @@ import os
 from markdownblocks import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -17,11 +17,16 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/',f'href="{basepath}')
+    template = template.replace('src="/',f'src="{basepath}')
 
     to_file = open(dest_path, "w")
     to_file.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+
+    if basepath == "":
+        basepath == "/"
     
     if not os.path.isdir(dir_path_content):
         print(f"Error: Source directory not found: {dir_path_content}")
@@ -33,13 +38,13 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         dst_path_item = os.path.join(dest_dir_path, item)
 
         if os.path.isdir(dir_path_item):
-            generate_pages_recursive(dir_path_item, template_path, dst_path_item)
+            generate_pages_recursive(dir_path_item, template_path, dst_path_item, basepath)
 
         elif os.path.isfile(dir_path_item):
             dst_page_dir = os.path.splitext(dst_path_item)[0]
             dst_page_path = dst_page_dir +".html"
 
-            generate_page(dir_path_item, template_path, dst_page_path)
+            generate_page(dir_path_item, template_path, dst_page_path, basepath)
 
 
 def extract_title(md):
